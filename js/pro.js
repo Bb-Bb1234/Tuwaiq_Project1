0337a89f9bd90b3560b01fe848029a9b1fc88504
Bb-Bb1234 <Badar.Salem.eam.js@tuwaiq.edu.sa>
2021-10-25 16:48:43 +0300

    project1

---
+++ js/pro.js
@@ -0,0 +1,19 @@
+// const indrued={
+//     name:"gg",
+//     price:"1000",
+
+// }
+// for (const key in indrued) {
+    
+// }
+
+$('#login').click(function() {
+    let userName = $('#username').val();
+    let password = $('#password').val();
+    
+    if (userName === "Bader" && password === "123") {
+        open('index.html');
+    }else{
+        alert("Wrong Data !!!");
+    }
+});
